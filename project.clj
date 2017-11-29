218e727cfb00881696d9421efc979f12024e4d23
Nicolas Berger <nicoberger@gmail.com>
2017-11-29 11:48:37 -0300

    Initial commit, empty clj project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject form-components "0.1.0-SNAPSHOT"
+  :description "Reagent form and UI components"
+  :url "https://github.com/jdkealy/form-components"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])

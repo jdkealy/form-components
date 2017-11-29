a366eb9408867133c614e60c0c475757c129b812
Nicolas Berger <nicoberger@gmail.com>
2017-11-29 12:56:24 -0300

    Add release config. Skip tag signing

--- project.clj
+++ project.clj
@@ -3,4 +3,14 @@
   :url "https://github.com/jdkealy/form-components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :deploy-repositories [["releases" :clojars]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
